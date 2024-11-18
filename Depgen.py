fcb5431fdbd51c8e7c20fb530b5ab6b58ed1bc0a
CLAUDIA ALVES DE OLIVEIRA <claoliva2010@gmail.com>
2024-11-18 11:36:44 -0300

    Update Depgen.py

--- Depgen.py
+++ Depgen.py
@@ -1,5 +1,25 @@
 import streamlit as st
 import pandas as pd
 import requests as rq
-dfconc=pd.concat([df, df1], axis='index')
-st.write('A distribuição de gênero na Câmara dos Deputados')
+url= 'https://dadosabertos.camara.leg.br/api/v2/deputados?siglaSexo=F&ordem=ASC&ordenarPor=nome'
+response=rq.get(url).json()
+response.keys()
+dfMulheres=pd.DataFrame(response['dados'])
+dfMulheres['sexo']='F'
+url= 'https://dadosabertos.camara.leg.br/api/v2/deputados?siglaSexo=M&ordem=ASC&ordenarPor=nome'
+dfHomens=pd.DataFrame()
+response=rq.get(url).json()
+dfHomens=pd.DataFrame(response['dados'])
+dfHomens['sexo']='M'
+dftotal=pd.concat([dfMulheres, dfHomens], axis='index')
+opcao=st.selectbox('Qual o sexo?', df['sexo'].unique())
+dfFiltrado=df[df['sexo']==opcao]
+st.title('Deputados do sexo' + opcao)
+ocorrencias=dfFiltrado['siglaUf'].value_counts()
+dfEstados=pd.DataFrame({'siglaUf': ocorrencias.index, 'quantidade': ocorrencias.values})
+totalHomens=dfHomens['nome'].count()
+st.metric('Total de homens', totalHomens)
+totalMulheres=dfMulheres['nome'].count()
+st.metric('Total de mulheres', totalMulheres)
+st.bar_chart(dfEstados, x='siglaUf', y='quantidade', x_label='Siglas dos Estados', y_label='Quantidade de Deputados')
+st.dataframe(dfFiltrado)
