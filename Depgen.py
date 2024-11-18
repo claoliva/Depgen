047d1af036d30a5ab382d20cebc30d417a63d18b
CLAUDIA ALVES DE OLIVEIRA <claoliva2010@gmail.com>
2024-11-18 11:39:44 -0300

    Update Depgen.py

--- Depgen.py
+++ Depgen.py
@@ -1,25 +1,39 @@
-import streamlit as st
+import requests as req
 import pandas as pd
-import requests as rq
-url= 'https://dadosabertos.camara.leg.br/api/v2/deputados?siglaSexo=F&ordem=ASC&ordenarPor=nome'
-response=rq.get(url).json()
-response.keys()
-dfMulheres=pd.DataFrame(response['dados'])
-dfMulheres['sexo']='F'
-url= 'https://dadosabertos.camara.leg.br/api/v2/deputados?siglaSexo=M&ordem=ASC&ordenarPor=nome'
-dfHomens=pd.DataFrame()
-response=rq.get(url).json()
-dfHomens=pd.DataFrame(response['dados'])
-dfHomens['sexo']='M'
-dftotal=pd.concat([dfMulheres, dfHomens], axis='index')
-opcao=st.selectbox('Qual o sexo?', df['sexo'].unique())
-dfFiltrado=df[df['sexo']==opcao]
+import streamlit as st
+urlMulheres = "https://dadosabertos.camara.leg.br/api/v2/deputados?siglaSexo=F&ordem=ASC&ordenarPor=nome"
+resposta = req.get(urlMulheres)
+dadosJSON = resposta.json()
+dfMulheres = pd.DataFrame(dadosJSON['dados'])
+dfMulheres['sexo'] = 'F'
+urlHomens = 'https://dadosabertos.camara.leg.br/api/v2/deputados?siglaSexo=M&ordem=ASC&ordenarPor=nome'
+resposta = req.get(urlHomens)
+dadosJSON = resposta.json()
+dfHomens = pd.DataFrame(dadosJSON['dados'])
+dfHomens['sexo'] = 'M'
+df = pd.concat([dfMulheres, dfHomens])
+
+opcao = st.selectbox(
+    'Qual o sexo?',
+    df['sexo'].unique()
+)
+dfFiltrado = df[df['sexo'] == opcao]
 st.title('Deputados do sexo' + opcao)
-ocorrencias=dfFiltrado['siglaUf'].value_counts()
-dfEstados=pd.DataFrame({'siglaUf': ocorrencias.index, 'quantidade': ocorrencias.values})
-totalHomens=dfHomens['nome'].count()
+
+ocorrencias = dfFiltrado['siglaUf'].value_counts()
+dfEstados = pd.DataFrame({
+    'siglaUf':ocorrencias.index,
+    'quantidade':ocorrencias.values}
+)
+
+totalHomens = dfHomens['id'].count()
 st.metric('Total de homens', totalHomens)
-totalMulheres=dfMulheres['nome'].count()
+totalMulheres = dfMulheres['id'].count()
 st.metric('Total de mulheres', totalMulheres)
-st.bar_chart(dfEstados, x='siglaUf', y='quantidade', x_label='Siglas dos Estados', y_label='Quantidade de Deputados')
+st.bar_chart(dfEstados,
+            x = 'siglaUf',
+            y = 'quantidade',
+            x_label = 'Siglas dos estados',
+            y_label = 'Quantidade de deputados'
+      )
 st.dataframe(dfFiltrado)
